5f9b97406afbfab0067732e9bcfbc207c6df491d
emmanuelkimoning968-dotcom <emmanuelkimoning968@gmail.com>
2026-08-21 10:28:54 +0300

    Fix src/main.jsx content

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
-src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Cashland from "./Cashland.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Cashland />
+  </React.StrictMode>
+);
